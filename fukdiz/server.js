54f35f4cf0639a5fa99b2e9e7ab2821b67e66413
Pat Shiu <shiu.patricia@gmail.com>
2016-01-21 02:18:50 -0500

    Produkitv yarrrr

--- fukdiz/server.js
+++ fukdiz/server.js
@@ -13,61 +13,52 @@ var app = new servi(true);
 port(3000);
 start();
 
-var db = useDatabase('people');
+var time = new Date().getTime();
+console.log(time);
+var name = 'people-'+time;
+var db = useDatabase(name);
 var links = [];
 
 var url = 'https://www.facebook.com/directory/people/';
 
-var rootListings = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "25"];
-
+var rootListings = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
+var lastIndex = rootListings.length;
 
 //track progress
-var current_level = 0;
+var currentLevel = 0;
 var currentLetter;
 var currentIndex = 0;
 
 
 var o = {
-	"level" : current_level,
-	"directories" : {
-		"directory" : [
-				//"directoryName" : "A", // A
-				//"listings" : []
-				]
-	}
-	
+	"level" : currentLevel,
+	"directories" :  [
+		//"directoryName" : "A", // A
+		//"listings" : []
+		]
 }
 
+currentLetter = rootListings[currentIndex];
 
-
-//LEVEL 0 Listings Index Iterator
-//for(var i = 0; i < rootListings.length; i++){
-for(var i = 0; i < 3; i++){
-	current_letter = rootListings[i];
-	addDirectory(current_letter);
-	if (i == 2){
-		writeToDatabase();
-	}
-}
+addDirectory(currentLetter);
 
 
 function addDirectory(dirName){
 	//Create new directory object and add to databas
-	var current_directory = {
+	var currentDirectory = {
 		"directoryName" : dirName,
 		"listings" : []
 	}
-	o.directories.directory.push(current_directory);
+	o.directories.push(currentDirectory);
 
 	var options = {
-		url: 'https://www.facebook.com/directory/people/'+current_letter,
+		url: 'https://www.facebook.com/directory/people/'+currentLetter,
 		headers: {
-			'User-Agent': 'google-analytics'
+			'User-Agent': 'google-analytics',
+			'encoding': 'utf8'
 		}
 	}
-	request(options, handleData);
-	console.log('Added \"' + dirName + '\" directory\'s listings. Current index: ' + currentIndex);
-	currentIndex++; 
+	request(options, handleData); //DONE
 }
 
 function handleData(error, response, body){
@@ -81,15 +72,28 @@ function handleData(error, response, body){
 			// console.log('text:',t);
 			// console.log('url:', u);
 
-			var current_object = {
+			var currentObject = {
 				"title" : t,
 				"url" : u
 			}
 
 			//this would temporarily save to the database
-			o.directories.directory[o.directories.directory.length-1].listings.push(current_object);
-			console.log("I'm doing something.");
+			o.directories[o.directories.length-1].listings.push(currentObject);
+			// console.log('Added listing:', o);
 	});
+
+	//move on to the next index
+	currentIndex++;
+	currentLetter = rootListings[currentIndex];
+
+	if(currentIndex < lastIndex){//if we still have indexes to find, keep scraping
+	//if(currentIndex < 2){
+		console.log('calling letter',currentLetter);
+		addDirectory(currentLetter);
+	}else{ //else write to database
+		writeToDatabase();
+	}
+		
 }
 
 function writeToDatabase(){
---
+++ notes/server.js
@@ -0,0 +1,127 @@
+var cheerio = require('cheerio');
+var request = require('request');
+
+
+// every servi application must have these 2 lines
+var servi = require("servi");
+var app = new servi(true);
+
+// set the port (defaults to 3000 if you leave out this line)
+port(3001);
+
+//track progress
+var currentLevel = 1; 
+var currentIndex = 0;
+
+//Load json DB
+var data = require('./people-1453356599569.json');
+console.log(data.directories.length);
+
+var dirIndex = 11; 
+
+var parentDirectoryURL = data.directories[dirIndex].listings[currentIndex].url;
+
+var time = new Date().getTime();
+//console.log(time);
+var name = 'listings-lvl'+currentLevel+'-'+data.directories[dirIndex].directoryName+'-'+currentIndex+'-'+time;  //!!!!!
+var db = useDatabase(name);
+
+
+//level
+//directory name (url)
+//	listings
+//		title
+//		url
+//		...
+
+//for each listing in the json file, create a db file with all the sub listings
+
+var o = {
+	"level" : currentLevel,
+	"directories" :  [
+		//"directoryName" : "A", // A
+		//"listings" : []
+		]
+}
+
+var startingIndex = 0; 
+//for each directory, create a new .db 
+//then load directory url, and add all sub-directories within that directory to the db
+
+addDirectory(parentDirectoryURL);
+
+function addDirectory(dirName){
+	//Create new directory object and add to databas
+	var currentDirectory = {
+		"directoryName" : dirName,
+		"listings" : []
+	}
+	o.directories.push(currentDirectory);
+
+	var options = {
+		url: parentDirectoryURL,
+		headers: {
+			'User-Agent': 'google-analytics',
+			'encoding': 'utf8'
+		}
+	}
+	request(options, handleData); //DONE
+}
+
+function handleData(error, response, body){
+	if (error) console.log(error); //Log Errors
+	$ = cheerio.load(body);
+
+	$('.fbDirectoryBoxColumn').find('.fbDirectoryBoxColumnItem').each(function(){
+		var u = $(this).find('a').attr('href');
+		var t = $(this).text();
+
+			// console.log('text:',t);
+			// console.log('url:', u);
+
+			var currentObject = {
+				"title" : t,
+				"url" : u
+			}
+
+			//this would temporarily save to the database
+			o.directories[o.directories.length-1].listings.push(currentObject);
+			// console.log('Added listing:', o);
+	});
+
+
+	//move on to the next index
+	currentIndex++;
+
+	if(currentIndex < data.directories[dirIndex].listings.length){//if we still have indexes to find, keep scraping
+		parentDirectoryURL = data.directories[dirIndex].listings[currentIndex].url;
+		console.log('adding directory #'+currentIndex+'-'+parentDirectoryURL);
+		addDirectory(parentDirectoryURL);
+	}else{ //else write to database
+		writeToDatabase();
+		console.log('successfully written to database #',dirIndex);
+		dirIndex++;
+		if(dirIndex < data.directories.length){
+			currentIndex = 0; 
+			parentDirectoryURL = data.directories[dirIndex].listings[currentIndex].url;
+			name = 'listings-lvl'+currentLevel+'-'+data.directories[dirIndex].directoryName+'-'+currentIndex+'-'+time;
+			db = useDatabase(name);
+			addDirectory(parentDirectoryURL);
+			console.log('finished writing to database, moving on to #'+dirIndex);
+		} else{
+			console.log('done!');
+		}
+	}		
+}
+
+function writeToDatabase(){
+	db.add(o);
+	console.log('Added to db.');
+}
+
+
+
+
+
+
+
